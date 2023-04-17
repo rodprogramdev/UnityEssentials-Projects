97dd9e6ee091f292538f6806aa46fe7ab8dcc8ba
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-17 15:29:39 +0800

    Add variable myMesssage

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    public string myMessage;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(myMessage);
     }
 }
